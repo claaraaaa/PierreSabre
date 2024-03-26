6c2324238dbcb55d56c02ffbe967686349a69391
claaraaaa <clara.gutierrez-alana@univ-tlse3.fr>
2024-03-26 19:15:28 +0100

    création de la classe Humain

---
+++ src/personnages/Humains.java
@@ -0,0 +1,64 @@
+package personnages;
+
+public class Humains {
+	private String nom;
+	private String boisson;
+	private int argent;
+	
+	public Humains(String nom, String boisson, int argent) {
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public int getArgent() {
+		return argent;
+	}
+	
+	private void parler(String texte) {
+		System.out.println("(" + nom + ") - " + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m’appelle " + nom + " et j’aime boire du " + boisson);
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (prix <= argent) {
+			parler("J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien + " à " + prix + " sous");
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien + " à " + prix + 
+					" sous.");
+		}
+		
+	}
+	
+	private void perdreArgent(int gain) {
+		argent -= gain;
+	}
+	
+	private void gagnerArgent(int gain) {
+		argent += gain;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Humains prof = new Humains("Prof", "kombucha", 54);
+		
+		prof.direBonjour();
+		prof.acheter("une boisson", 12);
+		prof.boire();
+		prof.acheter("un jeu", 2);
+		prof.acheter("un kimono", 50);
+	}
+}
